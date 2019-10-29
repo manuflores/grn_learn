9505a4fb5ef3bbb3cce46ff48d40e6e83663d8df
Manu Flores <emanuelflrsb@hotmail.com>
2019-10-29 15:23:42 -0700

    changed few things in viz.py and deleted the experiment folder

--- grn_learn/grn_learn/viz.py
+++ grn_learn/grn_learn/viz.py
@@ -1,28 +1,51 @@
+# +
+import matplotlib.pyplot as plt 
+import matplotlib as mpl
+import seaborn as sns
+
+def set_plotting_style():
+      
+    tw = 1.5
+    rc = {'lines.linewidth': 2,
+            'axes.labelsize': 18,
+            'axes.titlesize': 21,
+            'xtick.major' : 16,
+            'ytick.major' : 16,
+            'xtick.major.width': tw,
+            'xtick.minor.width': tw,
+            'ytick.major.width': tw,
+            'ytick.minor.width': tw,
+            'xtick.labelsize': 'large',
+            'ytick.labelsize': 'large',
+            'font.family': 'Helvetica',
+            'weight':'bold',
+            'grid.linestyle': ':',
+            'grid.linewidth': 1.5,
+            'grid.color': '#ffffff',
+            'mathtext.fontset': 'stixsans',
+            'mathtext.sf': 'fantasy',
+            'legend.frameon': True,
+            'legend.fontsize': 12, 
+           "xtick.direction": "in","ytick.direction": "in"}
+    
+    #plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
+    #plt.rc('mathtext', fontset='stixsans', sf='sans')
+    sns.set_style('ticks', rc=rc)
+    sns.set_palette("colorblind", color_codes=True)
+    sns.set_context('notebook', rc=rc)
+    
+
 def bokeh_style():
 
     '''
-    Formats bokeh plotting enviroment. Based on the RPgroup PBoC style.
+    Formats bokeh plotting enviroment.
+    Based on the RPgroup PBoC style created by G. Chure and M. Razo.
     '''
-    theme_json = {'attrs':
-            {'Figure': {
-                'background_fill_color': '#E3DCD0',
-                'outline_line_color': '#FFFFFF',
-            },
-            'Axis': {
-            'axis_line_color': "white",
-            'major_tick_in': 7,
-            'major_tick_line_width': 2.5,
-            'major_tick_line_color': "white",
-            'minor_tick_line_color': "white",
+    theme_json = {'attrs':{'Axis': {
             'axis_label_text_font': 'Helvetica',
             'axis_label_text_font_style': 'normal'
             },
-            'Grid': {
-                'grid_line_color': 'white',
-            },
             'Legend': {
-                'background_fill_color': '#E3DCD0',
-                'border_line_color': '#FFFFFF',
                 'border_line_width': 1.5,
                 'background_fill_alpha': 0.5
             },
@@ -31,7 +54,7 @@ def bokeh_style():
                'text_font': 'Helvetica'
             },
             'Title': {
-                'background_fill_color': '#FFEDC0',
+                #'background_fill_color': '#FFEDC0',
                 'text_font_style': 'normal',
                 'align': 'center',
                 'text_font': 'Helvetica',
@@ -39,3 +62,6 @@ def bokeh_style():
             }}}
 
     return theme_json
+# -
+
+
